3fa49b7c501644fb771882e7a90b7a2afc04fa21
GiorgiJulakidze1997 <g.julakidze1997@gmail.com>
2024-01-01 17:18:34 +0400

    added stylex of facebook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
+import * as stylex from '@stylexjs/stylex';
+
+
+const styles = stylex.create({
+  introduction: {
+    color: 'blue'.
+  },
+});
+
+
 
 function App() {
 
   return (
     <div>
 
-      <h1>
+      <h1 {...stylex.props(styles.heading)}>
         Coming soon ...
       </h1>
     </div>
